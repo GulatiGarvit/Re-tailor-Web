33873c941ab5a8aad9e0f5bf884a3726941b6568
Garvit Gulati <ggulati_be22@thapar.edu>
2023-05-05 23:25:15 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/retailor_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.retailor_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("HI!"),
+        ),
+      ),
+    );
+  }
+}
